72849e705b3506501117caf0cbe76e3de1dad762
tusharkhan <tushar.khan0122@gmail.com>
2021-05-08 22:52:48 +0600

    Update laravelthinq.php
    add username for auth

--- config/laravelthinq.php
+++ config/laravelthinq.php
@@ -4,4 +4,5 @@ return [
     'api_key' => env('THINQ_API_KEY'),
     'account_id' => env('THINQ_ACCOUNT_ID'),
     'disable_api_calls' => env('THINQ_DISABLE_API_CALLS', false),
+    'api_user' => env('THINQ_API_USERNAME')
 ];
